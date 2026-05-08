696f2e9b2a0e9fbf4c793484aea0fb9fe8e0de92
Arturo Mora-Rioja <arturomora@gmail.com>
2026-05-08 09:38:53 +0200

    Negative partitions added

--- test/driversLicense.test.js
+++ test/driversLicense.test.js
@@ -12,25 +12,25 @@ describe('Driver\'s license tests', () => {
          * Decision table-based test cases
          */
 
-        {'theory_exam_points': 90, 'practical_exam_errors': 1, 'output': {                               // Test case 1 based on business rule 1
+        {'theory_exam_points': 90, 'practical_exam_errors': 1, 'output': {  // Test case 1 based on business rule 1
             'license_granted': true,
             'repeat_theory_exam': false,
             'repeat_practical_exam': false,
             'additional_lessons': false
         }},
-        {'theory_exam_points': 90, 'practical_exam_errors': 5, 'output': {                               // Test case 2 based on business rule 2
+        {'theory_exam_points': 90, 'practical_exam_errors': 5, 'output': {  // Test case 2 based on business rule 2
             'license_granted': false,
             'repeat_theory_exam': false,
             'repeat_practical_exam': true,
             'additional_lessons': false
         }},
-        {'theory_exam_points': 50, 'practical_exam_errors': 1, 'output': {                               // Test case 3 based on business rule 3
+        {'theory_exam_points': 50, 'practical_exam_errors': 1, 'output': {  // Test case 3 based on business rule 3
             'license_granted': false,
             'repeat_theory_exam': true,
             'repeat_practical_exam': false,
             'additional_lessons': false
         }},
-        {'theory_exam_points': 50, 'practical_exam_errors': 5, 'output': {                               // Test case 4 based on business rule 4
+        {'theory_exam_points': 50, 'practical_exam_errors': 5, 'output': {  // Test case 4 based on business rule 4
             'license_granted': false,
             'repeat_theory_exam': true,
             'repeat_practical_exam': true,
@@ -43,7 +43,7 @@ describe('Driver\'s license tests', () => {
 
         // Theory exam points (a middle value is used for practical exam errors)
 
-        {'theory_exam_points': 0, 'practical_exam_errors': 1, 'output': {                               // Valid partition 0-84: lower boundary value
+        {'theory_exam_points': 0, 'practical_exam_errors': 1, 'output': {   // Valid partition 0-84: lower boundary value
             'license_granted': false,
             'repeat_theory_exam': true,
             'repeat_practical_exam': false,
@@ -55,19 +55,25 @@ describe('Driver\'s license tests', () => {
             'repeat_practical_exam': false,
             'additional_lessons': false
         }},
+        {'theory_exam_points': 2, 'practical_exam_errors': 1, 'output': {                               
+            'license_granted': false,
+            'repeat_theory_exam': true,
+            'repeat_practical_exam': false,
+            'additional_lessons': false
+        }},
         {'theory_exam_points': 83, 'practical_exam_errors': 1, 'output': {                               
             'license_granted': false,
             'repeat_theory_exam': true,
             'repeat_practical_exam': false,
             'additional_lessons': false
         }},
-        {'theory_exam_points': 84, 'practical_exam_errors': 1, 'output': {                               // Valid partition 0-84: upper boundary value
+        {'theory_exam_points': 84, 'practical_exam_errors': 1, 'output': {  // Valid partition 0-84: upper boundary value
             'license_granted': false,
             'repeat_theory_exam': true,
             'repeat_practical_exam': false,
             'additional_lessons': false
         }},
-        {'theory_exam_points': 85, 'practical_exam_errors': 1, 'output': {                               // Valid partition 85-100: lower boundary value
+        {'theory_exam_points': 85, 'practical_exam_errors': 1, 'output': {  // Valid partition 85-100: lower boundary value
             'license_granted': true,
             'repeat_theory_exam': false,
             'repeat_practical_exam': false,
@@ -85,7 +91,7 @@ describe('Driver\'s license tests', () => {
             'repeat_practical_exam': false,
             'additional_lessons': false
         }},
-        {'theory_exam_points': 100, 'practical_exam_errors': 1, 'output': {                              // Valid partition 85-100: upper boundary value           
+        {'theory_exam_points': 100, 'practical_exam_errors': 1, 'output': { // Valid partition 85-100: upper boundary value           
             'license_granted': true,
             'repeat_theory_exam': false,
             'repeat_practical_exam': false,
@@ -94,7 +100,7 @@ describe('Driver\'s license tests', () => {
 
         // Practical exam errors (a middle value is used for theory exam points)
 
-        {'theory_exam_points': 90, 'practical_exam_errors': 0, 'output': {                              // Partition 0-2: lower boundary value           
+        {'theory_exam_points': 90, 'practical_exam_errors': 0, 'output': {  // Partition 0-2: lower boundary value           
             'license_granted': true,
             'repeat_theory_exam': false,
             'repeat_practical_exam': false,
@@ -106,19 +112,25 @@ describe('Driver\'s license tests', () => {
             'repeat_practical_exam': false,
             'additional_lessons': false
         }},
-        {'theory_exam_points': 90, 'practical_exam_errors': 2, 'output': {                              // Partition 0-2: upper boundary value           
+        {'theory_exam_points': 90, 'practical_exam_errors': 2, 'output': {  // Partition 0-2: upper boundary value           
             'license_granted': true,
             'repeat_theory_exam': false,
             'repeat_practical_exam': false,
             'additional_lessons': false
         }},
-        {'theory_exam_points': 90, 'practical_exam_errors': 3, 'output': {                              // Partition 3-MAX INTEGER: lower boundary value           
+        {'theory_exam_points': 90, 'practical_exam_errors': 3, 'output': {  // Partition 3-MAX INTEGER: lower boundary value           
             'license_granted': false,
             'repeat_theory_exam': false,
             'repeat_practical_exam': true,
             'additional_lessons': false
         }},
         {'theory_exam_points': 90, 'practical_exam_errors': 4, 'output': {                              
+            'license_granted': false,
+            'repeat_theory_exam': false,
+            'repeat_practical_exam': true,
+            'additional_lessons': false
+        }},
+        {'theory_exam_points': 90, 'practical_exam_errors': 5, 'output': {                              
             'license_granted': false,
             'repeat_theory_exam': false,
             'repeat_practical_exam': true,
@@ -149,34 +161,50 @@ describe('Driver\'s license tests', () => {
 
     const driversLicenseFailsProvider = [
         
-        // Equivalence partition-based test case
+        //
+        // Equivalence partition-based test cases
+        //
 
-        {'theory_exam_points': 120, 'practical_exam_errors': 1},    // Theory exam points: middle value for the invalid partition
+        // Theory exam points: middle value for the invalid partition MIN PARTITION- -1
+        {'theory_exam_points': -50, 'practical_exam_errors': 1, 'exception': 'The number of exam points cannot be negative.'},    
 
-        // Boundary value-based test case
+        // Theory exam points: middle value for the invalid partition 101-MAX INTEGER
+        {'theory_exam_points': 120, 'practical_exam_errors': 1, 'exception': 'There can not be more than 100 points for a theory exam.'},    
 
-        {'theory_exam_points': 101, 'practical_exam_errors': 1},    // Theory exam points: lower boundary value for the invalid partition
+        // Practical exam errors: middle value for the invalid partition MIN INTEGER- -1
+        {'theory_exam_points': 90, 'practical_exam_errors': -5, 'exception': 'The number of practical exam errors cannot be negative.'},    
 
+        //
+        // Boundary value-based test cases
+        //
+
+        {'theory_exam_points': -2, 'practical_exam_errors': 1, 'exception': 'The number of exam points cannot be negative.'},    
+        
+        // Theory exam points: upper boundary value for the invalid partition MIN INTEGER- -1
+        {'theory_exam_points': -1, 'practical_exam_errors': 1, 'exception': 'The number of exam points cannot be negative.'},    
+        
+        // Theory exam points: lower boundary value for the invalid partition 101-MAX INTEGER
+        {'theory_exam_points': 101, 'practical_exam_errors': 1, 'exception': 'There can not be more than 100 points for a theory exam.'},    
+
+        // Practical exam errors: upper boundary value for the invalid partition MIN INTEGER- -1
+        {'theory_exam_points': 90, 'practical_exam_errors': -5, 'exception': 'The number of practical exam errors cannot be negative.'},    
+
+        //
         // Edge cases 
+        //
 
+        {'theory_exam_points': Number.MIN_SAFE_INTEGER, 'practical_exam_errors': 1},        // Minimum integer in JavaScript
+        {'theory_exam_points': Number.MIN_SAFE_INTEGER - 1, 'practical_exam_errors': 1},    // Minium integer in JavaScript - 1 
         {'theory_exam_points': Number.MAX_SAFE_INTEGER, 'practical_exam_errors': 1},        // Maximum integer in JavaScript
         {'theory_exam_points': Number.MAX_SAFE_INTEGER + 1, 'practical_exam_errors': 1},    // Maximum integer in JavaScript + 1 
     ];
     describe.each(driversLicenseFailsProvider)('Driver\'s license fails', (params) => {
         it(`${params.theory_exam_points} theory exam points, ${params.practical_exam_errors} practical exam errors fails`, () => {
-            expect(() => driversLicenseExamEvaluation(params.theory_exam_points, params.practical_exam_errors)).toThrow('There can not be more than 100 points for a theory exam.');
+            expect(() => driversLicenseExamEvaluation(params.theory_exam_points, params.practical_exam_errors)).toThrow(params.exception);
         });
     });
 
     // Other edge cases
-
-    it('Negative theory exam points fails', () => {
-        expect(() => driversLicenseExamEvaluation(-1, 1)).toThrow('The number of exam points cannot be negative.');
-    });
-
-    it('Negative practical exam errors fails', () => {
-        expect(() => driversLicenseExamEvaluation(90, -1)).toThrow('The number of practical exam errors cannot be negative.');
-    });
 
     const driversLicenseWrongDataTypeFailsProvider = [
         {'theory_exam_points': 'Hello', 'practical_exam_errors': 1},
